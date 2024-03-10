250f9734475e79909bf4f36b5953d7369800d265
Victor Maeiro Mendes da Silva <victor.silva@payly.com.br>
2024-03-10 15:01:25 -0300

    commit inicial

---
+++ escola-fenix-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Heading, Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p={4}>
+      <Heading>Hello, Chakra UI!</Heading>
+      <Button 
+      colorScheme="teal">Click me</Button>
+    </Box>
+  );
+}
+
+export default App;
